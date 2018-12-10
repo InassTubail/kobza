3907aa4899d82209e132b491e3feff9064e470f5
InassTubail <anoos.hanii@gmail.com>
2018-12-10 10:19:57 +0200

    dddddddddddddd

--- src/model/databse/db_connection.js
+++ src/model/databse/db_connection.js
@@ -3,7 +3,8 @@ const { Pool } = require('pg');
 
 require('env2')('./config.env');
 
-if (!process.env.DATABASE_URL) throw new Error('DB_URL is not set');
-const dbUrl = process.env.DATABASE_URL;
+if (!process.env.USERS_DB_URL) throw new Error('DB_URL is not set');
+const dbUrl = process.env.USERS_DB_URL;
 
 module.exports = new Pool({ connectionString: dbUrl, ssl: true });
+
